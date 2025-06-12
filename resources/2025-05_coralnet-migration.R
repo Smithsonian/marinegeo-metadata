5661a0aaf44db249d8ca6b457e8ac81b90165c72
mlonneman <mlonneman@gmail.com>
2025-06-12 08:49:59 -0400

    update taxonomy for coralnet migration

--- resources/2025-05_coralnet-migration.R
+++ resources/2025-05_coralnet-migration.R
@@ -30,3 +30,21 @@ write_csv(ids_out, "taxonomic-ids/marinegeo-taxonomic-ids.csv")
 
 # Get new classifications
 
+needed_classifications <- db_get_taxonomy_by_scientific_name(df_input$scientific_name) %>%
+  filter(is.na(level))
+
+new_classifications <- api_worms_get_classification(unique(needed_classifications$taxonomic_id))
+
+setdiff(colnames(new_classifications), colnames(needed_classifications))
+
+classifications_input <- read_csv("taxonomic-classifications/2025-04-01_marinegeo_classifications.csv")
+
+any(classifications_input$taxonomic_id %in% new_classifications$taxonomic_id)
+
+classifications_out <- classifications_input %>%
+  bind_rows(new_classifications) %>%
+  arrange(taxonomic_id)
+
+file.remove("taxonomic-classifications/2025-04-01_marinegeo_classifications.csv")
+
+write_csv(classifications_out, "taxonomic-classifications/marinegeo_classifications.csv")
