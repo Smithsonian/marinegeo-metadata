e95e069a5dce78c9789c54e1158353b2795fdc0b
Michael Lonneman <mlonneman@gmail.com>
2026-03-31 10:07:36 -0400

    add CHI-HKG metdata

---
+++ R/taxonomic-lookup-updates/add_new_IDs_taxonomic_lookup.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(taxize)
+
+obs_df <- read_csv(list.files("taxonomy-and-functional-groups/observation-lookup/", full.names = T))
+taxa_df <- read_csv("taxonomy-and-functional-groups/taxonomic-lookup/marinegeo_taxonomic_lookup.csv") 
+
+missing_taxa <- obs_df %>%
+  filter(!scientific_id %in% taxa_df$scientific_id) %>%
+  filter(str_starts(scientific_id, "APHIA:"))
+missing_taxa
+
+ids <- as.numeric(gsub("APHIA:", "", missing_taxa$scientific_id))
+ids
+
+out <- classification(ids, db="worms")
+out
+
+results <- bind_rows(
+  lapply(out, function(i){
+    i %>%
+      mutate(parent_id = case_when(
+        is.na(lag(id, n = 1)) ~ NA,
+        T ~ paste0("APHIA:", lag(id, n = 1))
+      ))
+  })
+) %>%
+  distinct() %>%
+  mutate(scientific_id = paste0("APHIA:", id)) %>%
+  select(scientific_id, everything())
+
+taxa_df_updated <- bind_rows(
+  results, taxa_df
+) %>%
+  arrange(id) %>%
+  distinct()
+
+write_csv(taxa_df_updated, "taxonomy-and-functional-groups/taxonomic-lookup/marinegeo_taxonomic_lookup.csv")
