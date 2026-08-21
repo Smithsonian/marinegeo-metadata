a3bc29145daec730fed940e7d286b7f93ccb8c54
mlonneman <mlonneman@gmail.com>
2026-08-21 16:11:49 -0400

    update taxonomic resources

--- tests/validate_functional_groups.R
+++ tests/validate_functional_groups.R
@@ -6,10 +6,10 @@ df <- read_csv("tests/oyster_height_scientific_names.csv")
 df_sci_ids <- df %>%
   mutate(scientific_id = marinegeo.utils::utl_mg_get_scientific_id(scientific_name))
 
-# Cliona = 132026
+# Cliona = urn:lsid:marinespecies.org:taxname:132026
 
-# Amphibalanus amphitrite = 421137
+# Amphibalanus amphitrite = urn:lsid:marinespecies.org:taxname:421137
 
-# Thoracica = 1107
+# Thoracica = urn:lsid:marinespecies.org:taxname:1107
 
-# Angulus texanus = 420862
+# Angulus texanus = urn:lsid:marinespecies.org:taxname:420862
