6de9ff9d6b1a54722cc6d5154691360871453428
mlonneman <mlonneman@gmail.com>
2026-09-01 15:47:43 -0400

    update fg tree

--- tests/validate_functional_groups.R
+++ tests/validate_functional_groups.R
@@ -9,13 +9,19 @@ df_sci_ids <- df %>%
 unique(marinegeo.utils::utl_mg_get_registry("functional_group_lookup")$tree_name)
 
 View(
-df_sci_ids %>%
-  mutate(category = marinegeo.utils::utl_mg_assign_functional_groups("oyster_density", 
-                                                                     c("Oysters",
-                                                                       "Non-oyster bivalves",
-                                                                       "Gastropods"), 
-                                                                     scientific_name))
+  df_sci_ids %>%
+    mutate(category = marinegeo.utils::utl_mg_assign_functional_groups("oyster_density", 
+                                                                       c("Oysters",
+                                                                         "Non-oyster bivalves",
+                                                                         "Gastropods",
+                                                                         "Other sessile invertebrates"), 
+                                                                       scientific_name))
 )
+
+tree <- marinegeo.utils::utl_mg_get_registry("functional_group_lookup") %>%
+  filter(tree_name == "oyster_density")
+
+utl_mg_get_functional_groups("Amphibalanus eburneus", "oyster_density")
 
 df_sci_ids %>%
   filter(!scientific_id %in% output_network_df$scientific_id)
